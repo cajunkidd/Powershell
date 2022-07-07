7df1740b91bb8a5301f554fdf63e1dc07a613fab
Tyler <60554951+softwaresuave@users.noreply.github.com>
2022-07-06 23:48:00 -0500

    Add files via upload

--- QCAutomationWork.ps1
+++ QCAutomationWork.ps1
@@ -22,98 +22,62 @@
 #>
 
 #Verify below paths to installers is correct!
-$NinjaLocation = "D:\QCAutomation\Ninja.exe"
-$AdobeLocation = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
-$ChromeLocation = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-$FirefoxLocation = "C:\Program Files\Mozilla Firefox\firefox.exe"
+$NinjaLocation         = "D:\QCAutomation\Ninja.exe"
+$AdobeLocation         = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
+$ChromeLocation        = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$FirefoxLocation       = "C:\Program Files\Mozilla Firefox\firefox.exe"
 $O365UninstallLocation = "D:\ODT\UninstallString.bat"
-$O365InstallLocation = "D:\ODT\O365std_x64.bat"
-
-$NewMachineName = Read-Host "Enter Computer Name:"
-$UnnecessaryApps   = @('*skypeapp*' , '*xbox*' , '*yourphone*' , '*solitairecollection*' , '*zune*', '*mcafee*' )
-$InstalledApps  = @()
-$MissingApps    = @()
-
-Import-Module -Name PSWindowsUpdate -Force -InformationAction SilentlyContinue
-$SoftwareInfo   = [PSCustomObject]@{
-    ComputerModel = Get-ComputerInfo -Property CsModel
-    ComputerSerialNumber = Get-ComputerInfo -Property BiosSeralNumber
-    NinjaInstalled = $False
-    NinajaVersion  = $null
+$O365InstallLocation   = "D:\ODT\O365std_x64.bat"
+$NewMachineName        = Read-Host "Enter Computer Name:"
+$UnnecessaryApps       = @('*skypeapp*' , '*xbox*' , '*yourphone*' , '*solitairecollection*' , '*zune*', '*mcafee*' )
+$InstalledApps         = @()
+$MissingApps           = @()
+$SoftwareInfo          = [PSCustomObject]@{
+    ComputerModel         = Get-ComputerInfo -Property CsModel
+    ComputerSerialNumber  = Get-ComputerInfo -Property BiosSeralNumber
+    NinjaInstalled        = $False
+    NinajaVersion         = $null
     MalwarebytesInstalled = $False
     MalwarebytesVersion   = $null
     CoNNectInstalled      = $False
     CoNNectVersion        = $null
-    AdobeInstalled = $False
-    AdobeVersion   = Get-ItemProperty -Path $AdobeLocation.ProductVersion
-    ChromeInstalled = $False
-    ChromeVersion   = $null
-    FirefoxInstalled = $False
-    FirefoxVersion   = $null
-    UninstalledApps  = $UninstalledApps
-    MissedBloatware  = $MissedBloatware
+    AdobeInstalled        = $False
+    AdobeVersion          = Get-ItemProperty -Path $AdobeLocation.ProductVersion
+    ChromeInstalled       = $False
+    ChromeVersion         = $null
+    FirefoxInstalled      = $False
+    FirefoxVersion        = $null
+    UninstalledApps       = $UninstalledApps
+    MissedBloatware       = $MissedBloatware
 }
 
+Import-Module -Name PSWindowsUpdate -Force -InformationAction SilentlyContinue
 Rename-Computer -NewName "$NewMachineName"
+
 #SoftwareUninstallandInstall
 foreach($App in $UnnecessaryApps){
-    Get-AppxPackage $App | Remove-AppxPackage -ErrorAction SilentlyContinue -InformationAction SilentlyContinue   
-    
-    if(test-path $AdobeLocation)  {
-        $SoftwareInfo.AdobeInstalled = $true
-           }
-
-        }
-<#
-Start-Process -Wait -FilePath D:\QCAutomation\UninstallString.bat | Write-Error -Message "ERROR DURING INSTALL OR NOT INSTALLED"
-Start-Process -Wait -FilePath 'D:\QCAutomation\Firefox Setup 102.0.exe' -ArgumentList '/s' -PassThru
-Start-Process -Wait -FilePath D:\ODT\O365std_x64.bat 
-
-Get-AppxPackage *skypeapp* | Remove-AppxPackage -ErrorAction SilentlyContinue
-Get-AppxPackage *xbox* | Remove-AppxPackage -ErrorAction SilentlyContinue -InformationAction SilentlyContinue
-Get-AppxPackage *yourphone* | Remove-AppxPackage -ErrorAction SilentlyContinue
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage -ErrorAction SilentlyContinue
-Get-AppxPackage *zune* | Remove-AppxPackage -ErrorAction SilentlyContinue
-Start-Sleep -Seconds 3
-#>
+    Get-AppxPackage $App | Remove-AppxPackage -ErrorAction SilentlyContinue -InformationAction SilentlyContinue
+    Get-AppxPackage $App | $UninstalledApps += $App
+    }  
+if(test-path $AdobeLocation){
+    $SoftwareInfo.AdobeInstalled = $true
+    }
+else{
+    Start-Process -Wait -FilePath D:\QCAutomation\AcroRdrDC2200120142_en_US.exe -ArgumentList '/sAll'      
+    }
+if(Test-Path $FirefoxLocation){
+    $SoftwareInfo.FirefoxInstalled = $True
+    }
+else{
+    Start-Process -Wait -FilePath 'D:\QCAutomation\Firefox Setup 102.0.exe' -ArgumentList '/s' -PassThru
+    }
+if(Test-Path $ChromeLocation){
+    $SoftwareInfo.ChromeInstalled = $True
+    $SoftwareInfo.ChromeVersion = Get-ItemProperty -Path $ChromeLocation.Product
+}
 #WindowsUpdate
 Write-Host -ForegroundColor Yellow "UPDATING WINDOWS...PLEASE WAIT"
 Install-WindowsUpdate -ForceDownload -ForceInstall
-<#
-#SoftwareChecks
-Write-Host -ForegroundColor Yellow "Checking for Installed Software"
-if(Test-Path 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'){
-    Write-Host -ForegroundColor Green "ADOBE READER IS INSTALLED"
-    (Get-ItemProperty -Path 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe').VersionInfo.ProductVersion
-    $InstalledApps += "Adobe"
-}
-else{
-    Write-Warning "ADOBE READER IS NOT INSTALLED"
-    Start-Process -Wait -FilePath D:\QCAutomation\AcroRdrDC2200120142_en_US.exe -ArgumentList '/sAll'
-    $MissingApps += "Adobe"
-}
-
-if(test-path 'C:\Program Files\Mozilla Firefox\firefox.exe'){
-    Write-Host -ForegroundColor Green "FIREFOX IS INSTALLED"
-    (Get-ItemProperty -Path 'C:\Program Files\Mozilla Firefox\firefox.exe').VersionInfo.ProductVersion
-    $InstalledApps += ",Firefox"
-}
-else{
-    Write-Warning "FIREFOX IS NOT INSTALLED"
-    $MissingApps += ",Firefox"
-}
-
-if(Test-Path 'C:\Program Files\Google\Chrome\Application\chrome.exe'){
-    Write-Host -ForegroundColor Green "CHROME IS INSTALLED"
-    (Get-ItemProperty -Path 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion
-    $InstalledApps += ",Chrome"
-}
-else{
-    Write-Warning "CHROME IS NOT INSTALLED"
-    $MissingApps += ",Chrome"
-}
-#>
-
 #End
 Write-Host -ForegroundColor Cyan "Checks Complete   Cleaning Up!"
 cleanmgr.exe /verylowdisk

75efde90d5597d2fa91c10c67d8c328f5c71cfdd
Tyler <60554951+softwaresuave@users.noreply.github.com>
2022-07-07 00:58:49 -0500

    Add files via upload

--- QCAutomationWork.ps1
+++ QCAutomationWork.ps1
@@ -26,39 +26,52 @@ $NinjaLocation         = "D:\QCAutomation\Ninja.exe"
 $AdobeLocation         = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
 $ChromeLocation        = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $FirefoxLocation       = "C:\Program Files\Mozilla Firefox\firefox.exe"
+$MalwareBytesLocation  = "C:\Program Files\Malwarebytes Endpoint Agent\UserAgent\Endpoint Agent Tray.exe"
 $O365UninstallLocation = "D:\ODT\UninstallString.bat"
 $O365InstallLocation   = "D:\ODT\O365std_x64.bat"
 $NewMachineName        = Read-Host "Enter Computer Name:"
 $UnnecessaryApps       = @('*skypeapp*' , '*xbox*' , '*yourphone*' , '*solitairecollection*' , '*zune*', '*mcafee*' )
-$InstalledApps         = @()
 $MissingApps           = @()
-$SoftwareInfo          = [PSCustomObject]@{
+$SoftwareInfo = [PSCustomObject]@{
     ComputerModel         = Get-ComputerInfo -Property CsModel
     ComputerSerialNumber  = Get-ComputerInfo -Property BiosSeralNumber
     NinjaInstalled        = $False
     NinajaVersion         = $null
     MalwarebytesInstalled = $False
-    MalwarebytesVersion   = $null
+    MalwarebytesVersion   = (Get-ItemProperty -Path "C:\Program Files\Malwarebytes Endpoint Agent\UserAgent\Endpoint Agent Tray.exe").VersionInfo
     CoNNectInstalled      = $False
-    CoNNectVersion        = $null
     AdobeInstalled        = $False
     AdobeVersion          = Get-ItemProperty -Path $AdobeLocation.ProductVersion
     ChromeInstalled       = $False
-    ChromeVersion         = $null
+    ChromeVersion         = Get-ItemProperty $ChromeLocation.ProductVersion
     FirefoxInstalled      = $False
-    FirefoxVersion        = $null
+    FirefoxVersion        = Get-ItemProperty $FirefoxLocation.ProductVersion
     UninstalledApps       = $UninstalledApps
     MissedBloatware       = $MissedBloatware
 }
-
 Import-Module -Name PSWindowsUpdate -Force -InformationAction SilentlyContinue
 Rename-Computer -NewName "$NewMachineName"
-
 #SoftwareUninstallandInstall
 foreach($App in $UnnecessaryApps){
     Get-AppxPackage $App | Remove-AppxPackage -ErrorAction SilentlyContinue -InformationAction SilentlyContinue
-    Get-AppxPackage $App | $UninstalledApps += $App
-    }  
+    Get-AppxPackage $App += $MissedBloatware
+    }
+Write-Host -ForegroundColor Yellow "Installing Office & Updating Windows wait..."
+Start-Process -Wait -FilePath D:\QCAutomation\UninstallString.bat | Write-Error -Message "ERROR - CHECK TO SEE IF ALREADY UNINSTALLED"
+Install-WindowsUpdate -ForceDownload -ForceInstall
+Start-Process -Wait -FilePath D:\ODT\O365std_x64.bat 
+#Install ninja
+if(Test-Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NinjaRMMAgent){
+    $SoftwareInfo.NinjaInstalled = $True
+    (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*NinjaRMMAgent*")
+}
+else{
+    Write-Warning "Ninja Agent Not Installed"
+    #instllninja
+}
+if(Test-Path -Path $MalwareBytesLocation){
+    $SoftwareInfo.Malwarebytesinstalled = $True
+}
 if(test-path $AdobeLocation){
     $SoftwareInfo.AdobeInstalled = $true
     }
@@ -74,12 +87,8 @@ else{
 if(Test-Path $ChromeLocation){
     $SoftwareInfo.ChromeInstalled = $True
     $SoftwareInfo.ChromeVersion = Get-ItemProperty -Path $ChromeLocation.Product
-}
-#WindowsUpdate
-Write-Host -ForegroundColor Yellow "UPDATING WINDOWS...PLEASE WAIT"
-Install-WindowsUpdate -ForceDownload -ForceInstall
+    }
 #End
-Write-Host -ForegroundColor Cyan "Checks Complete   Cleaning Up!"
 cleanmgr.exe /verylowdisk
 Remove-Item C:\users\nnadmin\Downloads\* -Force
 Clear-RecycleBin -Force
